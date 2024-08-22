b740df3b4728fae3ff0e001c54a78e0af7168fca
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2024-08-22 13:31:45 +0300

    [add] bloc state

---
+++ lib/src/bloc/internet_manager_state.dart
@@ -0,0 +1,27 @@
+// part of 'internet_manager_cubit.dart';
+//
+// @immutable
+// class InternetManagerState extends Equatable {
+//   const InternetManagerState._({
+//     this.state = InternetState.init,
+// });
+//
+//   const InternetManagerState.init() : this._();
+//
+//   /// #### This enum is used to show the current state of internet connection.
+//   ///
+//   /// * `init`: Initial state of the internet connection.
+//   /// * `checking`: The app is currently checking the internet connection.
+//   /// * `connected`: App is connected to the internet.
+//   /// * `disconnected`: App is not connected to the internet.
+//   ///
+//   /// You can use the `state` property to get the current state of the internet connection
+//   /// by calling `state.isChecking`, `state.isConnected`, and `state.isDisconnected` ...etc
+//   final InternetState state;
+//
+//
+//
+//   @override
+//   // todo: implement
+//   List<Object?> get props => throw UnimplementedError();
+// }
